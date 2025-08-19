c9a70ec99cf783159cb07cfdb9e3dbf53aa754ca
Pavan Bhogala <43247904+CKFVL@users.noreply.github.com>
2025-08-19 20:16:00 +0530

    Update this-constructor-function.js

--- this-constructor-function.js
+++ this-constructor-function.js
@@ -32,7 +32,7 @@ console.log("test global scope" + color)
 
 ######################
 // constructor function
-in both strict and non-strict mode, constructor function has access to `this` but the variables or objects mist be prefixed with this
+In both strict and non-strict mode, constructor function has access to `this` outside of the function but the variables or objects must be prefixed with this
 function BankAccount(newname, newage){
   // variable added to `this` scope can be accessed as `this.variable_name` OR `variable_name` within the object.
   this.name=newname; // if not bound to this, then it is not accessible for the object
