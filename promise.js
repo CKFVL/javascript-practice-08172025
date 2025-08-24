44676c0e604d32cb28bc5cddc088dcf867aa341c
Pavan Bhogala <43247904+CKFVL@users.noreply.github.com>
2025-08-24 19:34:01 +0530

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,17 @@
+to understand Javascript Eventloop, webapi, micro/macro task queues
+https://www.youtube.com/watch?v=eiC58R16hb8
+
+####################################################################
+Available promise state and values
+Pending -> undefined
+Fulfilled -> resolved value
+Rejected -> reason for rejection
+
+####################################################################
+Available promise methods:
+-  all
+-  allSettled
+-  race
+-  any
+
+####################################################################
