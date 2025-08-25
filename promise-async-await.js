ff122ac04e492e1d06c4d266573f9c04f36c34b2
Pavan Bhogala <43247904+CKFVL@users.noreply.github.com>
2025-08-25 08:10:03 +0530

    Update promise-async-await.js

--- promise-async-await.js
+++ promise-async-await.js
@@ -1,20 +1,74 @@
 to understand Javascript Eventloop, webapi, micro/macro task queues
 https://www.youtube.com/watch?v=eiC58R16hb8
 
-####################################################################
-Available promise state and values
-Pending -> undefined
-Fulfilled -> resolved value
-Rejected -> reason for rejection
+A promise in the javascript represents a single value that will be available now, in the future or never (in case of error)
+It's used to handle asynchronous operations
+States:
+  pending
+  fulfilled
+  rejected
 
+A promise emits only one value (or error)
+eager -> starts executing immediately when created
+cannot be cancelled once started
 ####################################################################
-Available promise methods:
--  all
--  allSettled
--  race
--  any
+Imagine we want to fetch user details, then fetch their orders, then fetch shipping info.
+Each step depends on the previous one. If any step fails, the chain should stop with an error.
 
-####################################################################
+Chaining promises:
+function fetchUser(userId){
+  
+  return new Promise((resolve, reject)=>{
+    setTimeout(function() {
+      if(userId===1){
+        resolve({'id':1, 'name': 'pavan', orderId: 21})
+      }else{
+        reject('user not found')
+      }
+    }, 1000);
+  })
+}
+
+function fetchOrders(orderId){
+  
+  return new Promise((resolve, reject)=>{
+    setTimeout(function(){
+      if(orderId===21){
+        resolve({'orderId': 21, 'details': 'iphone', 'paymentId': 11})
+      }else{
+        reject('order not found')
+      }
+    })
+  })
+}
+
+function fetchPayment(paymentId){
+  return new Promise(
+    (resolve, reject)=>{
+      if(paymentId===11){
+        resolve({'paymentId': 11, 'type': 'visa', 'expires': '11/2030'})
+      }else{
+        reject('payment not found')
+      }
+    }
+    )
+}
+fetchUser(1). // pass another value to fail
+then(val=>{
+  console.log(val)
+  return fetchOrders(21); // pass another value to fail
+}).
+then(orderval=>{
+  console.log(orderval)
+  return fetchPayment(11); // pass another value to fail
+}).
+then(paymentVal=>{
+  console.log(paymentVal)
+}).
+catch(error=> console.log(error)).finally('task completed')
+
+#############################################################
+
 function step1(value, error){
   return new Promise( (resolve, reject) => {
     if(!error){
