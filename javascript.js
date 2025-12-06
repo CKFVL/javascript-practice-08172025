b9b88ff21f6e34700f9508624c4803049da0ab0a
Pavan Bhogala <43247904+CKFVL@users.noreply.github.com>
2025-12-06 18:17:22 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,6 @@
+let a=6
+let b='2'
+const res=a/b // results=3 (javascript is weak type language i.e., the type is associated with run-time values)
+
+javascript is dynamic type language
+while typescript is static type language.
