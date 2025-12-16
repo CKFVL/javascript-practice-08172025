7f091a5d8670e44cea8297068c692bf88326be20
Pavan Bhogala <43247904+CKFVL@users.noreply.github.com>
2025-12-16 08:53:00 +0530

    Update hoisting.tsx

--- hoisting.tsx
+++ hoisting.tsx
@@ -19,3 +19,5 @@ Output:
 -------
 ReferenceError: Cannot access 'age' before initialization
 ##################
+https://chatgpt.com/g/g-p-6932cd86cb2481918db0c75be634dfea/c/6940ca43-4310-8324-84c1-e4db266fe357
+https://chatgpt.com/g/g-p-6932cd86cb2481918db0c75be634dfea/c/6940cc19-77ec-8323-bc1f-6c13bbae2226
