82b80f2bd8d79ca10a7d857bd2d4b37016a9a43f
Pavan Bhogala <43247904+CKFVL@users.noreply.github.com>
2025-12-17 11:24:18 +0530

    Create interview-questions.js

---
+++ interview-questions.js
@@ -0,0 +1,11 @@
+difference between regular function and arrow function?
+closures?
+function currying?
+  disadvantages of function currying?
+memoization?
+IIFE and setTimeout?
+module design pattern?
+promise? functions in promise?
+local storage and cookie?
+diff between var, let and const?
+hoisting?
