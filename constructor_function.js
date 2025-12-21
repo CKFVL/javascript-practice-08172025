42705f64d705c3b289670af25dc4c49e073bf869
Pavan Bhogala <43247904+CKFVL@users.noreply.github.com>
2025-12-21 23:10:48 +0530

    Update constructor_function.js

--- constructor_function.js
+++ constructor_function.js
@@ -1,4 +1,4 @@
-Environment	        Mode	          Global this (top-level)	            Function this (non-method)
+Environment	        Mode	         Global this (top-level)	           Function this (non-method)
 Browser	            Non-strict	    window(refers to global object)	      window
                     Strict	        undefined	                          undefined
 Node.js	            Non-strict	    module.exports(initially {})	      global
