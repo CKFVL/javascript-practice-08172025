2f9af6942d4ca0857144d0e38210e8eb776441e7
v-gurupavan.bhogala <v-gurupavan.bhogala@shutterfly.com>
2025-12-24 08:34:21 +0530

    updates

---
+++ memoization/memoization.js
@@ -0,0 +1,10 @@
+Memoization in JavaScript
+Memoization is an optimization technique where you cache the results of expensive function calls and return the cached result when the same inputs occur again.
+
+It’s commonly used for:
+
+Recursive algorithms (Fibonacci, factorial)
+
+Heavy computation functions
+
+Avoiding repeated API or database calls
