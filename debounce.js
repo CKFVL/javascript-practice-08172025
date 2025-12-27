a2bff8dc78819e383cbc89537834960bd4c04982
v-gurupavan.bhogala <v-gurupavan.bhogala@shutterfly.com>
2025-12-27 20:32:40 +0530

    updates

--- debounce.js
+++ debounce.js
@@ -1,3 +1,38 @@
+function search(str){
+  console.log(str)
+}
+
+// search('Ha')
+// search('Har')
+// search('Hard')
+// search('Hard')
+// search('Hard J') 
+// search('Hard JS')
+// search('Hard JS i')
+
+function debouncedSearch(fn, delay){
+let timer; // If You declared timer outside debouncedSearch:  
+// That makes timer global/shared, not tied to a specific debounced function.
+// Why this is a problem
+// If you create multiple debounced functions, they will interfere with each other
+// This breaks encapsulation and violates how debounce is expected to behave
+  return function(...args){
+    clearTimeout(timer)
+    timer=setTimeout(()=>{
+      fn(...args)
+    }, delay);
+  }
+}
+
+let ds=debouncedSearch(search, 2000)
+ds('Ha')
+ds('Har')
+ds('Hard')
+ds('Hard')
+ds('Hard J')
+ds('Hard JS')
+ds('Hard JS i')
+##########
 <!DOCTYPE html>
 <html>
   <head>
@@ -54,4 +89,3 @@ function debounce(fn, delay){
   }
   
 }
-
