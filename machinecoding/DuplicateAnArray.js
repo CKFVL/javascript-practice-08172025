3685ac9ba380fe5d0cefbf90d9286dac6b55ba71
v-gurupavan.bhogala <v-gurupavan.bhogala@shutterfly.com>
2026-01-11 00:23:30 +0530

    updates

---
+++ machinecoding/DuplicateAnArray.js
@@ -0,0 +1,9 @@
+const arr=[1,2,3,4,5]
+
+const duparr=[...arr, ...arr]
+console.log(duparr)
+
+##########
+const arr=[1,2,3,4,5]
+const duparr=arr.concat(arr)
+console.log(duparr)
---
+++ machinecoding/palindrome.js
@@ -0,0 +1,11 @@
+compare from beginning and end while looping
+
+#########
+const palstr='madam' // madam, level, noon
+
+const revstr=palstr.split("").reverse().join("")
+if(palstr===revstr){
+  console.log('yes')
+}else{
+  console.log('no')
+}
