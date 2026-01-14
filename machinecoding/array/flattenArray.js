3b6cb874666c691f0664d5fc96cf7ac9dd11c1dc
v-gurupavan.bhogala <v-gurupavan.bhogala@shutterfly.com>
2026-01-14 17:14:04 +0530

    updates

--- machinecoding/array/flattenArray.js
+++ machinecoding/array/flattenArray.js
@@ -18,3 +18,44 @@ function flattenArray(a){
 
 const res=flattenArray(a)
 console.log(res)
+#################################
+// let arr=[1,[2,3],[[4,5],6], 7, 8, [9,10]]
+
+// console.log(arr.flat()) // [ 1, 2, 3, [ 4, 5 ], 6, 7, 8, 9, 10 ] - 1 level only
+// console.log(arr.flatMap(x=>x)) // [ 1, 2, 3, [ 4, 5 ], 6, 7, 8, 9, 10 ] - 1 level only
+
+// console.log(arr.flat(Infinity)) // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
+
+let arr=[1,[2,3],[[4,5],6], 7, 8, [9,10]]
+// iterative - large/deep arrays
+function iterativeFlattening(narr){
+  let stack=[...narr]
+  let result=[]
+  
+  while(stack.length){
+    const next=stack.pop()
+    if(Array.isArray(next)){
+      stack.push(...next)
+    }else{
+      result.push(next)
+    }
+  }
+  console.log(result)
+  return result.reverse()
+}
+
+console.log(iterativeFlattening(arr))
+
+// recursive - works for any
+//
+function flatten(arr) {
+  return arr.reduce((acc, val) =>
+    Array.isArray(val)
+      ? acc.concat(flatten(val))
+      : acc.concat(val),
+    []
+  );
+}
+
+flatten([1, [2, [3, 4]], 5]);
+// [1, 2, 3, 4, 5]
