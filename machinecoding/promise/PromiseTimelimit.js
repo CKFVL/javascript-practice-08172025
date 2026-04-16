82c98ba9f9b909d9ffdf49efa354037262d4a391
v-gurupavan.bhogala <v-gurupavan.bhogala@shutterfly.com>
2026-04-17 00:44:11 +0530

    updates

--- machinecoding/promise/PromiseTimelimit.js
+++ machinecoding/promise/PromiseTimelimit.js
@@ -1,43 +1,29 @@
-// Promise time limit: throw error when time limit exceeds
+// Promise time limit: throw error when time limit exceeds to execute a function
 // reject after delay
 
-function rejectedFn(delay){
+// throw error when time limit reaches for execution of a function
+// slowFn: runs for certain time and succeeds
+// rejectFn: runs for certain time and rejects
+const rejectFn=(t)=>{
   return new Promise((_, reject)=>{
-          setTimeout(()=>reject('Time limit exceeded'), delay);
+    setTimeout(()=>reject('request timedout'), t);
   })
 }
 
-function promiseTimeLimit(fn, t){
+const slowFn=(fn, execTime)=>{
   return function(...args){
-    return Promise.race([
-      fn(...args),
-      rejectedFn(t)
-     ]
-    )
+    return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+        resolve(fn(...args))
+      }, execTime)
+    })  
+  }
+}
+const promiseTimeLimit=(slowFn, t)=>{
+  return function(...args){
+    return Promise.race([slowFn(...args), rejectFn(t)])  
   }
 }
 
-const sum=(a,b)=>a+b
-promiseTimeLimit(sum, 2000)(2,99)
-.then(data=>console.log(data))
-.catch(err=>console.log(err))
-//.finally(console.log('completed'))
-
-// Promise(res => setTimeout(res, 3000));
-// slow function
-
-function slowFn(fn, execTime){
-  return function(...args){
-  return new Promise(resolve=>{
-    setTimeout(()=>{
-      resolve(fn(...args))
-    }, execTime)
-    })
-}
-}
-//slowFn(sum)(4,54).then(data=>console.log(data)).catch(err=>console.log(err))
-
-promiseTimeLimit(slowFn(sum, 3000), 2000)(56,23)
-.then(data=>console.log(data))
-.catch(err=>console.log(err))
-//.finally(console.log('completed'))
+let p=promiseTimeLimit(slowFn((a,b)=>a+b, 3000), 2000)(2,3)
+p.then(res=>console.log(res)).catch(err=>console.log(err))
