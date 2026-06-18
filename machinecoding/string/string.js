4ea6dc59daa0bb0e1943a187e8c9e8f2b7961c49
CKFVL <pavanrya@gmail.com>
2026-06-18 10:42:43 +0530

    updates

--- machinecoding/string/string.js
+++ machinecoding/string/string.js
@@ -34,7 +34,18 @@ console.log(str.substring(3,-1)) // instead of starting from 3, it gets number o
 console.log(str.slice(6,-1)) // starts from 6 and eliminates characters from end provided by endIndex
 console.log(str.slice(6,-4))
 
+// object to string
+const user={name:'pavan'}
+const struser=JSON.stringify(user)
+console.log(struser)
 
+const parseobj=JSON.parse(struser)
+console.log(parseobj)
+
+// ascii
+const str='hello pavan'
+console.log(str.charCodeAt(2))
+console.log(String.fromCharCode(108))
 
 // usage of template string
 function test(subscribeStr, channel, person){
