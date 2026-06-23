f7fc4ec2a2dba956f4906d190c3911b7ca49de31
CKFVL <pavanrya@gmail.com>
2026-06-23 10:19:18 +0530

    kth positive integer

--- array/at.js
+++ array/at.js
@@ -1,8 +1,6 @@
-const arr=[4,-5,22, 3, 0, 30, 1,2,3,4,5]
-
-console.log(arr[2]) //22
-console.log(arr[arr.length-2]) // 4
-console.log(arr.at(2)) //22
-console.log(arr.at(arr.length-2)) // 4
+const arr2=[4,-5,22, 3, 0, 30, 1,2,3,44,5]
+console.log(arr2[2]) //22
+console.log(arr2[arr.length-2]) // 4
 // but
-console.log(arr.at(-0)) // 5
+console.log(arr2.at(-0)) // 4 which is 0
+console.log(arr2.at(-1)) // 5
--- machinecoding/search/findKthPositiveInteger.js
+++ machinecoding/search/findKthPositiveInteger.js
@@ -10,3 +10,29 @@
 // Output: 6
 // explanation: the missing positive integers are [5,6,8,....]. the 2nd missing positive integer is 6.
 
+let arr=[2,3,4,7,11] //[1,2,3,4]
+let i=0;
+let k=5 //2
+let currentK=0
+let j=1
+let kthMissingElementArray=[]
+while(currentK < k){ // 1
+    if(arr[i]!==j){
+        kthMissingElementArray.push(j); //1,5,6,8,9
+        currentK++; // 5
+        j++; //9
+    }else{
+        i++; //4
+        j++; //8
+    }
+
+}
+
+console.log(kthMissingElementArray.at(-0))
+
+const arr2=[4,-5,22, 3, 0, 30, 1,2,3,44,5]
+console.log(arr2[2]) //22
+console.log(arr2[arr.length-2]) // 4
+// but
+console.log(arr2.at(-0)) // 4
+console.log(arr2.at(-1))
