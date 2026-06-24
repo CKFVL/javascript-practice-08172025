218d6001c3356a79c529dfa9f20c0ad83302b2d2
CKFVL <pavanrya@gmail.com>
2026-06-24 10:45:57 +0530

    kth positive integer

--- machinecoding/search/MaximumCountOfPositiveAndNegativeIntegers.js
+++ machinecoding/search/MaximumCountOfPositiveAndNegativeIntegers.js
@@ -1,5 +1,6 @@
 // given an array nums sorted in non-decreasing order, return the max. between the number of positive integers and the number of negative integers
-// in other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the max. of pos and neg.
+// in other words, if the number of positive integers in nums is pos and the number of negative integers is neg, 
+// then return the max. of pos and neg.
 // note that 0 is neither positive nor negative
 
 // input: nums = [-2,-1,-1,1,2,3]
