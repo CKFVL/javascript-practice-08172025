76fa23635ded16421bb4ff9373cedc4cffa0a183
CKFVL <pavanrya@gmail.com>
2026-07-20 07:35:43 +0530

    two sum

---
+++ machinecoding/array/twosum.js
@@ -0,0 +1,5 @@
+/*
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    {2,11,15,7,9} targetsum=9; indices: 0 and 3
+    {2, 7, 11, 15}, target sum=9; indices 0 and 1
+*/
---
+++ machinecoding/string/LongestSubstringwithoutrepeatingcharacters.js
@@ -0,0 +1,7 @@
+// How do you find the longest substring without repeating characters?
+// **Sample Input:** "abcabcbb"
+// **Sample Output:** 3 (substring "abc")
+// **Sample Input:** "pwwkew"
+// **Sample Output:** 3 (substring "wke")
+
+
